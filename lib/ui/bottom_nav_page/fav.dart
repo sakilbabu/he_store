a3090107922a9aaa34647867d8bd45ee9fa01add
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-09-28 08:00:01 -0400

    "ui done"

--- lib/ui/bottom_nav_page/fav.dart
+++ lib/ui/bottom_nav_page/fav.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hestore/widgets/fetch_product.dart';
 
 class Favourite extends StatefulWidget {
+  const Favourite({Key? key}) : super(key: key);
+
   @override
   _FavouriteState createState() => _FavouriteState();
 }
